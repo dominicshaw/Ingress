a29ea582c44fbd9a8b1faa8dfb8c6a5f6a3a5b80
Dominic Shaw <dominicshaw@hotmail.com>
2018-04-27 09:24:52 +0100

    Added failing test

---
+++ Ingress.Tests/RepositoryTests.cs
@@ -0,0 +1,18 @@
+﻿using Ingress.Data.Interfaces;
+using Ingress.Data.Models;
+using Ingress.Data.Repositories;
+using NUnit.Framework;
+
+namespace Ingress.Tests
+{
+    [TestFixture]
+    public class RepositoryTests
+    {
+        [Test]
+        public void CanLoadAnything()
+        {
+            IAnalystMeetingRepository repo = new AnalystMeetingRepository(new IngressContext());
+            var data = repo.FindSkipped().GetAwaiter().GetResult();
+        }
+    }
+}
